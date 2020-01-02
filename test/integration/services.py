9bcdfaf6d982ff48cc8cbaf7408eb8c7d62f05c6
HadiZakiAlQattan <alqattanhadizaki@gmail.com>
2020-01-02 18:49:49 +0300

    integration tests <In progress>

---
+++ test/integration/services.py
@@ -0,0 +1,45 @@
+from urllib.parse import urljoin
+import requests
+
+# local import 
+from test.integration.constants import BASE_URL
+
+
+def get_configs():
+    """
+    return get request response with base_url 
+    """
+    response = requests.get(BASE_URL)
+    return response if response.ok else None
+
+
+def get_config(name:str):
+    """
+    return get request response with base_url + name
+    """
+    response = requests.get(urljoin(BASE_URL, name))
+    return response if response.ok else None
+
+
+def post_create_config(body:dict):
+    """
+    return post request response with base_url + body
+    """
+    response = requests.post(BASE_URL, json=body)
+    return response if response.ok else None
+
+
+def put_update_config(name:str, metadata:dict):
+    """
+    return put request response with base_url + name + body 
+    """
+    response = requests.put(urljoin(BASE_URL, name), json=metadata)
+    return response if response.ok else None
+
+
+def delete_config(name:str):
+    """
+    return delete request response with base_url + name
+    """
+    response = requests.delete(urljoin(BASE_URL, name))
+    return response if response.ok else None
